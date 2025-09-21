c6d3f1fb38df6ea7c337ec9a47a27e71b812c086
ariframaps <ariframaputra33@gmail.com>
2025-09-21 22:14:17 +0700

    auth page layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,46 @@
+import Image from "next/image";
 import styles from "./layout.module.scss";
+import GlobeIcon from "@/components/svg/GlobeIcon";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <header className={`${styles.header}`}>
-        <span className={`${styles.header__logo}`}>Goforumroh</span>
+    <div className={`${styles.container}`}>
+      <header
+        className={`${styles.header} bg-brand-blue_whale-color-01 text-neutral-white`}>
+        <Image
+          className={`${styles.header__logo}`}
+          src={"/Logo.png"}
+          alt="Go For Umrah Logo"
+          width={146}
+          height={26}
+        />
         <div className={`${styles.header__info}`}>
-          <button className={`${styles.header__info__languageBtn}`}>
-            <span className="text-success-main font-bold">En</span>
+          <button className={`${styles.header__languageBtn}`}>
+            <GlobeIcon width={24} height={24} />
+            <span>En</span>
           </button>
-          <button className={`${styles.header__info__helpBtn}`}>Help</button>
+          <span className="h-full w-[1px] bg-neutral-subtle"></span>
+          <button className="px-[24px] py-[12px] border-[1.5px] border-brand-green-color-01 rounded-[8px]">
+            Help
+          </button>
         </div>
       </header>
-      <section>{children}</section>
-    </>
+      <main className={`${styles.main}`}>
+        <section className={`${styles.main__children}`}>
+          {children}
+          <p className="text-neutral-subtle">
+            All rights reserved. Copyright 2022 – GoForUmrah.com™
+          </p>
+        </section>
+        <Image
+          className={`${styles.main__banner}`}
+          src={"/auth-banner.png"}
+          alt={"Go For Umrah"}
+          width={500}
+          height={500}
+          priority
+        />
+      </main>
+    </div>
   );
 }
--- src/components/svg/GlobeIcon.tsx
+++ src/components/svg/GlobeIcon.tsx
@@ -3,19 +3,16 @@ import { SVGProps } from "react";
 const GlobeIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
     fill="none"
+    stroke="currentColor"
     {...props}>
     <path
-      stroke="#fff"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={1.5}
       d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18ZM3.513 9h16.974M3.513 15h16.975"
     />
     <path
-      stroke="#fff"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={1.5}
